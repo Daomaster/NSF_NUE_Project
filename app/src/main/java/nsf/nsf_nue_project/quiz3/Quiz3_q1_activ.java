236831df97b091ff91c2c354d0f694d99f910982
marcosddourado <marcos_bdrlr@hotmail.com>
2015-10-27 10:19:50 -0700

    quiz 3 re-dimensioned.

--- app/src/main/java/nsf/nsf_nue_project/quiz3/Quiz3_q1_activ.java
+++ app/src/main/java/nsf/nsf_nue_project/quiz3/Quiz3_q1_activ.java
@@ -1,15 +1,17 @@
 package nsf.nsf_nue_project.quiz3;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import nsf.nsf_nue_project.R;
-
 
 public class Quiz3_q1_activ extends ActionBarActivity {
 
@@ -44,6 +46,7 @@ public class Quiz3_q1_activ extends ActionBarActivity {
         opD.setText(questionArray1[4]);
         opE.setText(questionArray1[5]);
 
+        setTextDimensions();
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,4 +69,41 @@ public class Quiz3_q1_activ extends ActionBarActivity {
             }
         });
     }
+
+    private void setTextDimensions() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenHeight = size.y;
+        int btnTxtSize = (int) (screenHeight * 0.03);
+        int questionMargin = (int) (screenHeight * 0.15);
+        int answerMargin = (int) (questionMargin/3);
+
+        questionTxt.setTextSize(btnTxtSize);
+        setMargins(questionTxt, questionMargin, questionMargin, 0, questionMargin / 2);
+
+        opA.setTextSize(btnTxtSize);
+        setMargins(opA, answerMargin, 0, 0, 0);
+
+        opB.setTextSize(btnTxtSize);
+        setMargins(opB, answerMargin, 0, 0, 0);
+
+        opC.setTextSize(btnTxtSize);
+        setMargins(opC, answerMargin, 0, 0, 0);
+
+        opD.setTextSize(btnTxtSize);
+        setMargins(opD, answerMargin, 0, 0, 0);
+
+        opE.setTextSize(btnTxtSize);
+        setMargins(opE, answerMargin, 0, 0, 0);
+    }
+
+    public void setMargins (View view, int left, int top, int right, int bottom) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            view.requestLayout();
+        }
+    }
+
 }
--- app/src/main/java/nsf/nsf_nue_project/quiz3/Quiz3_q2_activ.java
+++ app/src/main/java/nsf/nsf_nue_project/quiz3/Quiz3_q2_activ.java
@@ -1,9 +1,12 @@
 package nsf.nsf_nue_project.quiz3;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -46,8 +49,7 @@ public class Quiz3_q2_activ extends ActionBarActivity {
         opD.setText(questionArray[4]);
         opE.setText(questionArray[5]);
 
-        Intent intent = getIntent();
-        score = intent.getIntExtra("score", 0);
+        setTextDimensions();
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,4 +74,44 @@ public class Quiz3_q2_activ extends ActionBarActivity {
             }
         });
     }
+
+    private void setTextDimensions() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenHeight = size.y;
+        int btnTxtSize = (int) (screenHeight * 0.03);
+        int questionMargin = (int) (screenHeight * 0.15);
+        int answerMargin = (int) (questionMargin/3);
+
+        questionTxt.setTextSize(btnTxtSize);
+        setMargins(questionTxt, questionMargin, questionMargin, 0, questionMargin / 2);
+
+        opA.setTextSize(btnTxtSize);
+        setMargins(opA, answerMargin, 0, 0, 0);
+
+        opB.setTextSize(btnTxtSize);
+        setMargins(opB, answerMargin, 0, 0, 0);
+
+        opC.setTextSize(btnTxtSize);
+        setMargins(opC, answerMargin, 0, 0, 0);
+
+        opD.setTextSize(btnTxtSize);
+        setMargins(opD, answerMargin, 0, 0, 0);
+
+        opE.setTextSize(btnTxtSize);
+        setMargins(opE, answerMargin, 0, 0, 0);
+
+        Intent intent = getIntent();
+        score = intent.getIntExtra("score", 0);
+    }
+
+    public void setMargins (View view, int left, int top, int right, int bottom) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            view.requestLayout();
+        }
+    }
+
 }
